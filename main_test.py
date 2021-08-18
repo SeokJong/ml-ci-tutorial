80719f184fd53fbc9c2083d3e47e3c5eff1ca757
SeokJong <psj216@gmail.com>
2021-08-18 16:41:50 +0900

    gitignore

---
+++ main_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+import main
+
+
+class MainTest(unittest.TestCase):
+    def test_hello(self):
+        ret = main.hello("Test")
+        self.assertEqual(ret,"hello world: Test1")
+
+if __name__ == "__main__":
+    unittest.main()
